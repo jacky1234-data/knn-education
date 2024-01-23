2a1f07d7e0d5c82eeefd9b907a0e8202f9c95356
Jacky Liu <87030427+jacky1234-data@users.noreply.github.com>
2024-01-22 19:37:14 -0500

    Add files via upload

---
+++ templates/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Heroku PHP Example</title>
+</head>
+<body>
+    <h1>Hello from Heroku PHP!</h1>
+    
+    <?php
+        // Include the content from content.html
+        include 'index.html';
+    ?>
+    
+    <p>This is additional content in the PHP file.</p>
+</body>
+</html>
